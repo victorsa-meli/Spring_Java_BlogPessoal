1f04f8de01e3b03539e1fe8cc50aca698d41f43e
VictorSa <victor.rsa@mercadolivre.com>
2022-11-23 16:42:45 -0300

    Corrigindo @Repository

--- blogpessoal/src/main/java/com/mercadolivre/blogpessoal/repository/PostagemRepository.java
+++ blogpessoal/src/main/java/com/mercadolivre/blogpessoal/repository/PostagemRepository.java
@@ -2,8 +2,9 @@ package com.mercadolivre.blogpessoal.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.mercadolivre.blogpessoal.model.Postagem;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
 }
