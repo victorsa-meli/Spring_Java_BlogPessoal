3593fbf21368ba7abb7187f45a0e1f7a51085314
VictorSa <victor.rsa@mercadolivre.com>
2022-12-08 12:21:46 -0300

    CriandoTestesJunit

--- src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
+++ src/main/java/com/generation/blogpessoal/controller/UsuarioController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("/usuario")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
 
--- src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -38,8 +38,8 @@ public class BasicSecurityConfig {
 
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .antMatchers("/usuarios/logar").permitAll()
-                        .antMatchers("/usuarios/cadastrar").permitAll()
+                        .antMatchers("/usuario/logar").permitAll()
+                        .antMatchers("/usuario/cadastrar").permitAll()
                         .antMatchers(HttpMethod.OPTIONS).permitAll()
                         .anyRequest().authenticated())
                 .httpBasic();
